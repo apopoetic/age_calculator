dce4c5157ead95bc0c3e44fb54b865d6687c4836
apopoetic <ankurjamwal@live.com>
2022-02-09 13:35:36 +0530

    age calculator

---
+++ age_calculator.R
@@ -0,0 +1,15 @@
+#simple age calculator
+library(lubridate)
+
+#enter date of birth in yyyy-mm-dd format
+dob <- ymd("1986-08-24")
+
+age_on_today <- today()
+
+#enter any other date and remove # below
+#age_on <- ymd("yyyy-mm-dd")
+
+#time interval between dob and today
+age_int <- interval(ankur, age_on_today) #replace age_on_today with age_on if required
+
+(age <- as.period(age_int))
